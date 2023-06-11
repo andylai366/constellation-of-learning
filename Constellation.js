d9b9778a8f74e77abbe2618648212576cd6761b5
AndyL366 <andylai366@gmail.com>
2023-06-11 18:07:41 -0400

    Smoothness

--- Constellation.js
+++ Constellation.js
@@ -37,7 +37,7 @@ const handleOnMove = e => {
     
     const percentage = (mouseDelta / maxDelta) * -100,
           nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-          nextPercentage = Math.round(Math.max(Math.min(nextPercentageUnconstrained, 0), -100) * 1000000) / 1000000;
+          nextPercentage = Math.round(Math.max(Math.min(nextPercentageUnconstrained, 0), -100) * 1000) / 1000;
     
     track.dataset.percentage = nextPercentage; 
 
